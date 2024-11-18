2dc31275790bc54ab6ec679b612d166929383947
2Axelle9 <axelle.hannier@gmail.com>
2024-11-18 21:05:12 +0100

    Ajout du contenu a la page A propos

--- aPropos.php
+++ aPropos.php
@@ -22,24 +22,47 @@
 <?php include_once "header.php" ?>
 <section>
     <H2>LE BUREAU</H2>
+    <H3>Edition 2024</H3>
+    <img src="assets/photo_bureau_ADIIL.png" alt="Photo de groupe du bureau du BDE 2024" />
+    <p>Le Bureau des Étudiants "ADIIL" du département informatique de l'IUT de Laval a pour ambition de créer un environnement stimulant et convivial pour tous les étudiants. Notre motivation principale est de favoriser les échanges, de créer des opportunités d’apprentissage et de renforcer les liens entre les membres de notre communauté. Nous organisons des événements variés, allant des ateliers techniques aux soirées conviviales, pour répondre aux intérêts de chacun.</p>
+</section>
+
+<section>
+    <H2>LES MEMBRES</H2>
+    
+    <div>
+        <p>Enzo, notre Président : </p>
+        <p>Passionné par l’informatique et le travail d’équipe, Enzo est un leader naturel. Il est motivé par l’idée de créer un cadre où chaque étudiant peut s’épanouir. Toujours à l'écoute, il s'assure que toutes les voix sont entendues et que nos projets reflètent les attentes de nos membres.</p>
+        <img src="assets/photo_enzo.png" alt="Photo de Enzo Rynders--Vitu" />
+    </div>
 
     <div>
-        <H3>Edition 2024</H3>
-        <img src="assets/photo_bureau_ADIIL.png" alt="Photo de groupe du bureau du BDE 2024" />
+        <p>Tom, le Comptable : </p>
+        <p>Avec un œil attentif aux détails et une grande rigueur, Tom gère nos finances avec sérieux. Sa motivation vient de son désir d’optimiser les ressources pour que chaque événement soit un succès. Il sait que la bonne gestion est la clé de notre développement.</p>
+        <img src="assets/photo_tom.png" alt="Photo de Tom Ysope" />
     </div>
+
+    <div>
+        <p>Mathis, Chargé de communication : </p>
+        <p>Toujours à la recherche de nouvelles façons de communiquer, Mathis a un talent pour la cuisine et la création de contenu. Sa motivation est de faire briller notre BDE et d'informer les étudiants sur nos activités. Il utilise les réseaux sociaux pour créer une véritable communauté en ligne.</p>
+        <img src="assets/photo_mathis.png" alt="Photo de Mathis Le Nôtre" />
+    </div>
+
+    <div>
+        <p>Gémino, Secrétaire : </p>
+        <p>Gémino joue un rôle essentiel dans le bon fonctionnement du BDE. Sa motivation réside dans le fait de s'assurer que tout est bien structuré, de la prise de notes lors des réunions à la gestion des documents administratifs. Il aime voir les choses s'organiser efficacement.</p>
+        <img src="assets/photo_gemino.png" alt="Photo de Gemino Ruffault--Ravenel" />
+    </div>
+
+    <div>
+        <p>Axel & Julien, Membres : </p>
+        <p>Axel et Julien sont les nouvelles recrues pleines d'énergie et d'idées innovantes. Ils sont motivés par le désir d’apporter une touche fraîche à nos événements et de fédérer les étudiants autour de projets communs. Leur enthousiasme contagieux aide à dynamiser notre équipe et à créer une atmosphère joyeuse.</p>
+        <img src="assets/photo_axel.png" alt="Photo de Enzo Rynders--Vitu" />
+        <img src="assets/photo_julien.png" alt="Photo de Julien Dauvergne" />
+    </div>
+
 </section>
 
-<p>Le Bureau des Étudiants "ADIIL" du département informatique de l'IUT de Laval a pour ambition de créer un environnement stimulant et convivial pour tous les étudiants. Notre motivation principale est de favoriser les échanges, de créer des opportunités d’apprentissage et de renforcer les liens entre les membres de notre communauté. Nous organisons des événements variés, allant des ateliers techniques aux soirées conviviales, pour répondre aux intérêts de chacun.</p>
-
-</section>
-
-<H2>LES MEMBRES</H2>
-
-<div>
-    <p>Enzo, notre Président : </p>
-    <p>Passionné par l’informatique et le travail d’équipe, Enzo est un leader naturel. Il est motivé par l’idée de créer un cadre où chaque étudiant peut s’épanouir. Toujours à l'écoute, il s'assure que toutes les voix sont entendues et que nos projets reflètent les attentes de nos membres.</p>
-    <img src="assets/photo_enzo.png" alt="Photo de Enzo Rynders--Vitu" />
-</div>
 
 </body>
 </html>
