a2d9383629765c4a27ef92f3c5cbb0cd09247dd0
Julien DAUVERGNE <judau27@gmail.com>
2024-11-18 13:16:14 +0100

    database.php fontionel & hedear v2

--- header.php
+++ header.php
@@ -1,27 +1,35 @@
+<?php
+session_start();
+$isUserLoggedIn = isset($_SESSION['user']); // Vérifie si l'utilisateur est connecté
+?>
+
 <!-- HEADER -->
 <header>
-      <a id="accueil" href="./index.html">
-        <img src="assets/logo.png" alt="Logo de l'ADIIL"> </a>
-      <nav>
+    <a id="accueil" href="./index.html">
+        <img src="assets/logo.png" alt="Logo de l'ADIIL">
+    </a>
+    <nav>
         <ul>
-          <li>
-            <a href="./evenements.html">Evenements</a>
-          </li>
-          <li>
-            <a href="./actualites.html">Actualités</a>
-          </li>
-          <li>
-            <a href="./boutique.html">Boutique</a>
-          </li>
-          <li>
-            <a href="./agenda.html">Agenda</a>
-          </li>
-          <li>
-            <a href="./aPropos.html">À propos</a>
-          </li>
-          <li>
-            <a href="./monCompte.html">Mon compte</a>
-          </li>
+            <li>
+                <a href="./evenements.html">Événements</a>
+            </li>
+            <li>
+                <a href="./actualites.html">Actualités</a>
+            </li>
+            <li>
+                <a href="./boutique.html">Boutique</a>
+            </li>
+            <?php if ($isUserLoggedIn): ?>
+                <li>
+                    <a href="./agenda.html">Agenda</a>
+                </li>
+            <?php endif; ?>
+            <li>
+                <a href="./aPropos.html">À propos</a>
+            </li>
+            <li>
+                <a href="./monCompte.html">Mon compte</a>
+            </li>
         </ul>
-      </nav>
+    </nav>
 </header>
