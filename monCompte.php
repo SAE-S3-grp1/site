30a5561b2ffba0c18dd9a6a2eced99f5cdeeffaf
Julien DAUVERGNE <judau27@gmail.com>
2024-11-19 14:46:05 +0100

    block 2 index et changement marges body (id)

--- monCompte.php
+++ monCompte.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="public_style.css">
 </head>
-<body>
+<body class="body_margin">
 
 <!-- HEADER -->
 <?php require_once "header.php" ?>
