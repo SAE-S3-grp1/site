87ab55e670f5bfa352bb952c702cc6830dd9d9b3
Julien DAUVERGNE <judau27@gmail.com>
2024-11-20 09:47:53 +0100

    import des polices dans le css + détails section 3 index

--- aPropos.php
+++ aPropos.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -10,7 +6,6 @@
     <title>À propos</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="public_style.css">
 </head>
 
--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    
+    <title>Accueil</title>
     <link rel="stylesheet" href="public_style.css">
 </head>
 <body id="index" class="body_margin">
