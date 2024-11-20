20a5677fc3c285b5839d1ae1d1ca864cd99e71b9
2Axelle9 <axelle.hannier@gmail.com>
2024-11-20 16:43:39 +0100

    Remaniement de monCompte et suppression du style

--- monCompte.php
+++ monCompte.php
@@ -37,20 +37,98 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!-- PARTIE MON COMPTE -->
 
 <H2>MON COMPTE</H2>
-<section>
-    <div id=carreCompte>
-        
-        <div id="account-info">
-                <div id="cadre-photo">
-                    <img src="assets/photo_mathis.png" alt="Photo de profil de l'utilisateur"/>
-                </div>
-                <p>210 XP</p>
-                <div>
-                    <p>Grade diamant</p>
-                    
-                </div>
+<section> <!-- Ensemble des différents formulaires du compte -->
+    <div id="account-generalInfo">
+        <form method="POST" action="" id="account-profilPhoto-form">
+            <div id="cadre-photo">
+                <img src="assets/photo_mathis.png" alt="Photo de profil de l'utilisateur"/>
+            </div>
+        </form>
+        <div>
+            <p>210</p>
+            <p>XP</p>
+        </div>
+        <div>
+            <p>Grade diamant</p>
+            <img src="assets/grade_diamant.png" alt="Illustration du grade diamant"/>
+        </div>
+    </div>
+
+    <form method="POST" action="" id="account-personalInfo-form">
+        <input type="text" id="name" name="name" placeholder="Prénom"required>
+
+        <input type="text" id="lastName" name="lastName" placeholder="Nom de famille"required>
+
+        <select id="tp" name="tp">
+            <option value="11A">TP 11 A</option>
+            <option value="11B">TP 11 B</option>
+            <option value="12C">TP 12 C</option>
+            <option value="12D">TP 12 D</option>
+            <option value="21A">TP 21 A</option>
+            <option value="21B">TP 21 B</option>
+            <option value="22C">TP 22 C</option>
+            <option value="22D">TP 22 D</option>
+            <option value="31A">TP 31 A</option>
+            <option value="31B">TP 31 B</option>
+            <option value="32C">TP 32 C</option>
+            <option value="32D">TP 32 D</option>
+        </select>
+    </div>
+    
+    <form method="POST" action="" id="account-loginInfo-form">
+        <input type="email" id="mail" name="mail" placeholder="Adresse mail" required>
+
+        <div>
+            <input type="password" id="mdp" name="mdp" placeholder="Mot de passe actuel" required>
+            <input type="password" id="newMdp" name="newMdp" placeholder="Nouveau mot de passe" required>
+            <input type="password" id="newMdp" name="newMdp" placeholder="Confirmation du nouveau mot de passe" required>
+        </div>
+    </div>
+
+<section> <!-- Ensemble des différents boutons du compte -->
+    
+    <!--Discord-->
+    <button type="button">
+        <a href="https://discord.com" target="_blank">
+            <img src="assets/logo_discord.png" alt="Logo de Discord">
+            Associer à Discord
+        </a>
+    </button>
+
+    <!--Deconnexion-->
+    <form action="" method="post">
+    <input type="hidden" name="deconnexion" value="true">
+    <button type="submit">Déconnexion</button>
+    </form>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        if($_POST['deconnexion']){
+        unset($_SESSION['userid']);
+        $message = "Vous avez été déconnecté.";
+        }
+        echo $message;
+    }
+    ?>
+
+    <!--Supprimer son compte-->
+    <button type="button">
+        <a href="supprimer_compte.php" target="_blank">
+            Supprimer mon compte
+        </a>
+    </button>
 
 
+
+
+
+
+
+
+
+
+<!--
                 <button type="button">
                     <a href="https://discord.com" target="_blank">
                     <img src="assets/logo_discord.png" alt="Logo de Discord">
@@ -101,7 +179,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 
 </section>
-
+-->
 
 <!-- FOOTER -->
 <?php require_once "footer.php" ?>
