ec5cfd5dad7a166151f40f85e2924ab807290f25
Julien DAUVERGNE <judau27@gmail.com>
2024-11-26 15:48:22 +0100

    filepicker api 2

--- files_save.php
+++ files_save.php
@@ -1,19 +1,28 @@
-<?php 
+<?php
 
-public static function saveFile() : File | null
+function generateUUID(){
+        $data = random_bytes(16);
+
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+        return bin2hex($data);
+}
+
+function saveFile() : string | null
     {
         // Retourne le nom du fichier si l'enregistrement a réussi, faux sinon.
 
         $name = tools::generateUUID() . '.' . pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 
-        if (move_uploaded_file($_FILES['file']['tmp_name'], 'files/' . $name)) {
-            return new File($name);
+        if (move_uploaded_file($_FILES['file']['tmp_name'], '/api/files/' . $name)) {
+            return $name;
         }
 
         return null;
     }
 
-    public static function saveImage() : File | null
+function saveImage() : string | null
     {
         // Vérification des données de l'image, puis enregistrement.
         // Retourne Faux si l'image n'en est pas une, ou si elle n'a pas pu être enregistrée.
@@ -31,7 +40,7 @@ public static function saveFile() : File | null
         }
 
         // On s'assure que l'extension du fichier ne causerait pas de problèmes
-        return self::saveFile();
+        return saveFile();
     }
 
 ?>
