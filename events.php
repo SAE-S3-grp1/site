899a2b3fb13b7667820cd8b58400ade1b8f330b2
Julien DAUVERGNE <judau27@gmail.com>
2024-11-28 19:19:28 +0100

    update

--- events.php
+++ events.php
@@ -3,18 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-    <title>Evenements</title>
-    
+    <title>Les Evenements</title>
+
     <link rel="stylesheet" href="styles/general_style.css">
     <link rel="stylesheet" href="styles/header_style.css">
     <link rel="stylesheet" href="styles/footer_style.css">
-
     <link rel="stylesheet" href="styles/events_style.css">
-
 </head>
 <body>
 <?php
@@ -22,48 +19,43 @@
     require_once 'database.php';
     $db = new DB();
     $isLoggedIn = isset($_SESSION["userid"]);
+    $show = 2;
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['show']) && is_numeric($_GET['show']) && $_GET['show'] > 3) {
-        $show_number = 8 + (int) $_GET['show'];
-    } else {
-        $show_number = 8;
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['show']) && is_numeric($_GET['show'])) {
+        $show = (int) $_GET['show'];
     }
 ?>
 <h1>LES EVENEMENTS</h1>
 <section>
+    <a class="show-more" href="events.php?show=<?php echo $show + 5; ?>">Voir Plus d'Anciens Evenements</a>
     <div class="events-display">
                 <?php
                     $date = getdate();
                     $sql_date = $date["year"]."-".$date["mon"]."-".$date["mday"];
-
                     $joursFr = [0 => 'Dimanche', 1 => 'Lundi', 2 => 'Mardi', 3 => 'Mercredi', 4 => 'Jeudi', 5 => 'Vendredi', 6 => 'Samedi'];
                     $moisFr = [1 => 'Janvier', 2 => 'Février', 3 => 'Mars', 4 => 'Avril', 5 => 'Mai', 6 => 'Juin', 7 => 'Juillet', 8 => 'Août', 9 => 'Septembre', 10 => 'Octobre', 11 => 'Novembre', 12 => 'Décembre'];
                     $current_date = new DateTime(date("Y-m-d"));
 
-
                     $events_to_display = $db->select(
-                        "SELECT id_evenement, nom_evenement, lieu_evenement, date_evenement FROM EVENEMENT WHERE date_evenement >= ? ORDER BY date_evenement ASC LIMIT ? ;",
-                        "si",
-                        [$sql_date, $show_number]
-                    );
-                    $passed_events = $db->select(
-                        "SELECT id_evenement, nom_evenement, lieu_evenement, date_evenement FROM EVENEMENT WHERE date_evenement < ? ORDER BY date_evenement ASC LIMIT 2;",
+                        "SELECT id_evenement, nom_evenement, lieu_evenement, date_evenement FROM EVENEMENT WHERE date_evenement >= ? ORDER BY date_evenement ASC;",
                         "s",
                         [$sql_date]
                     );
+                    $passed_events = $db->select(
+                        "SELECT id_evenement, nom_evenement, lieu_evenement, date_evenement FROM EVENEMENT WHERE date_evenement < ? ORDER BY date_evenement ASC LIMIT ?;",
+                        "si",
+                        [$sql_date, $show]
+                    );
                     $events_to_display = array_merge($passed_events, $events_to_display);
-
 
                 foreach ($events_to_display as $event):
                     $eventid = $event["id_evenement"];
-
                     $event_date = substr($event['date_evenement'], 0, 10);
                     $event_date_info = getdate(strtotime($event_date));
-
                     $event_date = new DateTime($event_date);
                     $other_classes = "";
                     $isPassed = false;
-                    
+
                     if ($event_date < $current_date) {
                         $date_pin_class = "passed";
                         $date_pin_label = "Passé";
@@ -72,32 +64,25 @@
                     } elseif ($event_date == $current_date) {
                         $date_pin_class = "today";
                         $date_pin_label = "Aujourd'hui";
-                    }else{
+                    } else {
                         $date_pin_class = "upcoming";
                         $date_pin_label = "A venir";
                     }
                     ?>
                     <div class="event-box <?php echo "$other_classes";?>">
-
                         <div class="timeline-event">
-                            
                             <h4> <?php echo ucwords($joursFr[$event_date_info['wday']]." ".$event_date_info["mday"]." ".$moisFr[$event_date_info['mon']]);?></h4>
-
                             <div class="vertical-line"></div>
-                            
                             <p> <?php echo "$date_pin_label";?></p>
-
                             <div class="timeline-marker <?php echo " $date_pin_class" ?>">
                                 <div class="time-line"></div>
                             </div>
                         </div>
-
                         <div class="event" event-id="<?php echo $eventid;?>">
                             <div>
                                 <h2><?php echo $event['nom_evenement'];?></h2>
                                 <?php echo ucwords($event["lieu_evenement"]);?>
                             </div>
-
                             <h4
                                 <?php
                                 $isPlaceDisponible = $db->select(
@@ -106,11 +91,9 @@
                                     [$eventid])[0]['isPlaceDisponible'];
                                 
                                 if($isPlaceDisponible){
-                                    //editable
                                     $event_subscription_color_class = "event-not-subscribed hover_effect";
                                     $event_subscription_label = "S'inscrire";
                                 }else{
-                                    //editable
                                     $event_subscription_color_class = "event-full";
                                     $event_subscription_label = "Complet";
                                 }
@@ -123,7 +106,6 @@
                                     ));
                                     
                                     if($isSubscribed){
-                                        //editable
                                         $event_subscription_color_class = "event-subscribed";
                                         $event_subscription_label = "Inscrit";
                                     }
@@ -136,17 +118,13 @@
                                 echo "class=\"$event_subscription_color_class\"";
                                 ?>>
                                 <?php echo $event_subscription_label;?>
-
                             </h4>
                         </div>
                     </div>
                 <?php endforeach; ?>
         </div>
-        <a class="show-more" href="events.php?show=<?php echo $show_number + 5; ?>">Voir Plus</a>
 </section>
-
     <?php require_once 'footer.php';?>
     <script src="/scripts/event_details_redirect.js"></script>
-
 </body>
 </html>
