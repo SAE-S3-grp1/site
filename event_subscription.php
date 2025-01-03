3e455f69a9efc95a5fbd20e164aa75ffc99ed7a3
Julien DAUVERGNE <judau27@gmail.com>
2025-01-03 17:11:11 +0100

    inscription ok

--- event_subscription.php
+++ event_subscription.php
@@ -1,47 +1,62 @@
 <?php
 session_start();
 $isLoggedIn = isset($_SESSION["userid"]);
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["eventid"]) && $isLoggedIn) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $isLoggedIn) {
 
     $userid = $_SESSION["userid"];
     $eventid = $_POST["eventid"];
 
     require_once 'database.php';
     $db = new DB();
-    $event = $db->select(
-        "SELECT nom_evenement, xp_evenement, prix_evenement, reductions_evenement FROM EVENEMENT WHERE id_evenement = ? ;",
-        "i",
-        [$eventid]
-    );
-    if(empty($event)){
-        header("Location: index.php");
+    if(isset($_POST["price"], $_POST["eventid"])){
+        $db->query(
+            "INSERT INTO `INSCRIPTION` (`id_membre`, `id_evenement`, `date_inscription`, `paiement_inscription`, `prix_inscription`)
+            VALUES (?, ?, NOW(), 'WEB', ?);",
+            "iid",
+            [$userid, $eventid, $_POST["price"]]
+        );
+        header("Location: events.php");
         exit;
     }
-    $event = $event[0];
-    $title = $event["nom_evenement"];
-    $xp = $event["xp_evenement"];
-    $price = $event["prix_evenement"];
+    elseif(isset($_POST["eventid"])){
+            $event = $db->select(
+                "SELECT nom_evenement, xp_evenement, prix_evenement, reductions_evenement FROM EVENEMENT WHERE id_evenement = ? ;",
+                "i",
+                [$eventid]
+            );
+            if(empty($event)){
+                header("Location: index.php");
+                exit;
+            }
+            $event = $event[0];
+            $title = $event["nom_evenement"];
+            $xp = $event["xp_evenement"];
+            $price = $event["prix_evenement"];
 
-    $isDiscounted = boolval($event["reductions_evenement"]);
-    $user_reduction = 1;
+            $isDiscounted = boolval($event["reductions_evenement"]);
+            $user_reduction = 1;
 
-    if($isDiscounted){
-        $user_reduction = $db->select(
-            "SELECT reduction_grade FROM ADHESION 
-             JOIN GRADE ON ADHESION.id_grade = GRADE.id_grade
-             WHERE id_membre = ? AND reduction_grade > 0 order by ADHESION.date_adhesion DESC LIMIT 1",
-             "i",
-             [$userid]
-        );
-        if(!empty($user_reduction)){
-            $user_reduction = 1 - ($user_reduction[0]["reduction_grade"]/100);
+            if($isDiscounted){
+                $user_reduction = $db->select(
+                    "SELECT reduction_grade FROM ADHESION 
+                    JOIN GRADE ON ADHESION.id_grade = GRADE.id_grade
+                    WHERE id_membre = ? AND reduction_grade > 0 order by ADHESION.date_adhesion DESC LIMIT 1",
+                    "i",
+                    [$userid]
+                );
+                if(!empty($user_reduction)){
+                    $user_reduction = 1 - ($user_reduction[0]["reduction_grade"]/100);
+                }
+            }
+            $price = $price*$user_reduction;
+        }else{
+            header("Location: login.php");
+            exit;
         }
+    }else{
+        header("Location: login.php");
+        exit;
     }
-
-}else{
-    header("Location: login.php");
-    exit;
-}
 ?>
 
 <!DOCTYPE html>
@@ -55,7 +70,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["eventid"]) && $isLogg
 <body>
     <div>
         <h1><?php echo strtoupper($title)?></h1>
-        <h2><?php echo $price*$user_reduction." €"?></h2>
+        <h2><?php echo $price." €"?></h2>
+        <br>
+        <p>Page de présentation : Les éléments de payement sont pûrement décoratifs.</p>
     </div>
    
 
@@ -83,7 +100,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["eventid"]) && $isLogg
             <button type="button">Se connecter à PayPal</button><br><br>
         </div>
 
-        <button type="submit">Valider l'Abonnement</button>
+        <input type="hidden" name="eventid" value="<?php echo $eventid?>">
+        <input type="hidden" name="price" value="<?php echo $price?>">
+        <!-- Logique pour verifier la validité avec banque / paypal (normalement) -->
+        <button type="submit">Valider l'Inscription</button>
     </form>
 
     <script>
