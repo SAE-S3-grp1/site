2ce2fe725e6070cf6e3e028d65f4c017cca181e1
Julien DAUVERGNE <judau27@gmail.com>
2025-01-05 17:00:04 +0100

    agenda ok

---
+++ agenda.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+    
+</head>
+<body>
+    <iframe 
+        src="https://edt.gemino.dev" 
+        style="width: 99vw; height: 500vh; border: none; margin:auto; align-self:center;display:flex;">
+    </iframe>
+</body>
+</html>
