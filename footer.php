ba90734150b4859d0bd93b10e36ee50d26ccf8ef
2Axelle9 <axelle.hannier@gmail.com>
2025-01-07 10:12:21 +0100

    Suppression liens footer

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <!-- HEADER -->
 <footer>
     <div>
-        <a href="contact.php">Nous contacter</a>
+        <a>Nous contacter</a>
         <a href="about.php">Nous rejoindre</a>
         <a href="news.php">Le département</a>
-        <a href="legal_mentions.php">Mentions légales</a>
+        <a>Mentions légales</a>
     </div>
     <h2>ADIIL</h2>
 </footer>
